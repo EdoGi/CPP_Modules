85a162e3470ed42af3e7c356d4318fac17154b24
Edoardo GIACOMINI <egiacomi@e1r7p2.clusters.42paris.fr>
2022-11-22 02:52:39 +0100

    push 100% mais faux"

--- Mod2/ex03/srcs/bsp.cpp
+++ Mod2/ex03/srcs/bsp.cpp
@@ -2,23 +2,34 @@
 
 Fixed area(Point const A, Point const B, Point const C)
 {
-   return (A.getX() * (B.getY() - C.getY()) + B.getX() * (C.getY() - A.getY()) + C.getX() * (A.getY() - C.getY())/2);
+	Fixed tmp(A.getX() * (B.getY() - C.getY()) + B.getX() * (C.getY() - A.getY()) + C.getX() * (A.getY() - C.getY())/2);
+	Fixed sign(-1);
+
+	if (tmp < 0)
+		tmp = tmp * sign;
+	return (tmp);
 }
 
 bool bsp(Point const A, Point const B, Point const C, Point const P)
-{  
-   /* Calculate area of triangle ABC */
-   Fixed M = area(A, B, C);
-  
-   /* Calculate area of triangle PBC */ 
-   Fixed A1 = area (P, B, C);
-  
-   /* Calculate area of triangle PAC */ 
-   Fixed A2 = area (A, P, C);
-  
-   /* Calculate area of triangle PAB */  
-   Fixed A3 = area (A, B, P);
-   
-   /* Check if sum of A1, A2 and A3 is same as A */
-   return (M == A1 + A2 + A3);
+{
+	/* Calculate area of triangle ABC */
+	Fixed M = area(A, B, C);
+	std::cout << "M is : " <<  M << std::endl;
+
+	/* Calculate area of triangle PBC */ 
+	Fixed A1 = area(P, B, C);
+	std::cout << "A1 is : " <<  A1 << std::endl;
+
+	/* Calculate area of triangle PAC */ 
+	Fixed A2 = area(A, P, C);
+	std::cout << "A2 is : " << A2 << std::endl;
+	
+	/* Calculate area of triangle PAB */  
+	Fixed A3 = area(A, B, P);
+	std::cout << "A3 is : " <<  A3 << std::endl;
+
+	/* Check if sum of A1, A2 and A3 is same as A */
+	Fixed tmp(A1.toInt() + A2.toInt() + A3.toInt());
+	std::cout << "sum is : " << tmp << std::endl;
+	return (M.toInt() == A1.toInt() + A2.toInt() + A3.toInt());
 }
